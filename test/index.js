5fc67a566c145b23ffdb3b2f64589d8c46f07ed7
Lukasz Goworko <lgo@haffmansneumeister.com>
2018-05-09 10:06:40 +0200

    ✅ tests for Select:isCollapes

--- test/index.js
+++ test/index.js
@@ -712,6 +712,34 @@ test("Select:nullOption", function(tape) {
   );
 });
 
+test("Select:isCollapsed:true", function(tape) {
+  tape.plan(1);
+
+  tape.strictEqual(
+    new Select({
+      type: Country,
+      options: { isCollapsed: true },
+      ctx: ctx
+    }).getLocals().isCollapsed,
+    true,
+    "should handle help option"
+  );
+});
+
+test("Select:isCollapsed:false", function(tape) {
+  tape.plan(1);
+
+  tape.strictEqual(
+    new Select({
+      type: Country,
+      options: { isCollapsed: false },
+      ctx: ctx
+    }).getLocals().isCollapsed,
+    false,
+    "should handle help option"
+  );
+});
+
 var Checkbox = core.Checkbox;
 
 test("Checkbox:label", function(tape) {
